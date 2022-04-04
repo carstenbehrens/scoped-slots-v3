f67aa27db6e81f85f05ca8fe2bab9de28096bf80
carstenbehrens <carsten.behrens1@googlemail.com>
2022-04-04 17:03:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <child-component v-slot="slotProps">
+    Now we can use the data from the child component here: {{ slotProps.example }}
+  </child-component>
+</template>
+
+<script setup>
+import ChildComponent from './components/ChildComponent.vue'
+</script>
